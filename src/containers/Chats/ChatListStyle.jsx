328e4163618f18b07e582d49cf918bc913740c4d
alex16leiva <alex16leiva@gmail.com>
2025-04-28 20:38:57 -0600

    Estilos en el mensajes tipo

--- src/containers/Chats/ChatListStyle.jsx
+++ src/containers/Chats/ChatListStyle.jsx
@@ -10,12 +10,15 @@ export const ChatTypeSelected = styled.div`
     display: flex;
     justify-content: space-around;
     font-weight: bold;
+    color: rgba(98, 82, 82, 0.6);
     border-bottom: 3px solid transparent;
     background-color: #f0f2f5;
     padding: 10px;
 
     &.selected {
         border-bottom: 3px solid #FF4081; /* Color del borde cuando está seleccionado */
-        background-color: #ddd
+        background-color: #ddd;
+        font-weight: bold;
+        color: black;
     }
 `
